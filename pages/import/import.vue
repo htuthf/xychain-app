60255db578ab296fe772fc9aaba0215c695ffef5
kt <>
2026-02-11 10:40:21 +0900

    feat:

--- pages/import/import.vue
+++ pages/import/import.vue
@@ -26,8 +26,8 @@
 					backdropFilter: 'blur(2px)',
 					webkitBackdropFilter: 'blur(2px)'
 				},
-				// inputWords:[ 'piece', 'aspect', 'cabbage', 'utility', 'own', 'vivid', 'front', 'volcano', 'sell', 'kick','into', 'shop']
-				inputWords: []
+				inputWords:[ 'piece', 'aspect', 'cabbage', 'utility', 'own', 'vivid', 'front', 'volcano', 'sell', 'kick','into', 'shop']
+				// inputWords: []
 			}
 		},
 		computed: {
